85fb563a002dd923d5c04468cd0aa548ceb70c51
LR <lucie.ruzickova86@gmail.com>
2025-06-09 12:47:50 +0200

    dotazy

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,26 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        String jmeno = "Jan Novak";
-        LocalDate datumNarozeni;
-
+        /* puvodni verze kde nastavuji rucne promenou
+        String jmeno; // nastaveni jmena
+        LocalDate datumNarozeni;  // definice atributu datumNarozeni
         int pocetSjednanychSmluv = 10;
         float prodanaMrkev = 10.5F;
         boolean senior = true;
 
-        Prodejce prodejce1 = new Prodejce();
+        */
+
+
+        Prodejce prodejce1 = new Prodejce(); // Proc davam Prodejce a pak promenou prodejce1 nasledovuje odkaz na tridu
         Prodejce prodejce2 = new Prodejce();
 
-        prodejce1.setJmeno("Jan Novak \t");
-        prodejce1.setDatumNarozeni(LocalDate.of(1955, 11, 22));
-        System.out.println(prodejce1.getJmeno());
-        //System.out.println(prodejce1.getJmeno() + prodejce1.getDatumNarozeni()); Pro vypsani data narozeni
+        prodejce1.setJmeno("Jan Novak \t");  // /t dela tabulatorovy odskok /n dela novy radek
+        prodejce1.setDatumNarozeni(LocalDate.of(1955, 11, 22));  //nastavuji  prodejce1
+
+        // prodejce1.setPocetSjednanychSmluv(1);
+
+        System.out.println(prodejce1.getJmeno()); // vypsani jen jmena prodejce
+        //System.out.println(prodejce1.getJmeno() + prodejce1.getDatumNarozeni()); //Pro vypsani data narozeni
 
 
         prodejce2.setJmeno("Josef Novy \t");
