8cad2e31000cbdeb45995ef2efcee266749b7f9b
LR <lucie.ruzickova86@gmail.com>
2025-06-10 19:59:50 +0200

    oprava tab mimo setter prodejce

--- src/Main.java
+++ src/Main.java
@@ -19,16 +19,16 @@ public class Main {
         Prodejce prodejce1 = new Prodejce(); // Proc davam Prodejce a pak promenou prodejce1 nasledovuje odkaz na tridu
         Prodejce prodejce2 = new Prodejce();
 
-        prodejce1.setJmeno("Jan Novak \t");  // /t dela tabulatorovy odskok /n dela novy radek
+        prodejce1.setJmeno("Jan Novak");  // /t dela tabulatorovy odskok /n dela novy radek
         prodejce1.setDatumNarozeni(LocalDate.of(1955, 11, 22));  //nastavuji  prodejce1
 
         // prodejce1.setPocetSjednanychSmluv(1);
 
-        System.out.println(prodejce1.getJmeno()); // vypsani jen jmena prodejce
-        //System.out.println(prodejce1.getJmeno() + prodejce1.getDatumNarozeni()); //Pro vypsani data narozeni
+        // System.out.println(prodejce1.getJmeno()); // vypsani jen jmena prodejce
+        System.out.println(prodejce1.getJmeno() + "\t"+  prodejce1.getDatumNarozeni()); //Pro vypsani data narozeni
 
 
-        prodejce2.setJmeno("Josef Novy \t");
+        prodejce2.setJmeno("Josef Novy ");
         prodejce2.setDatumNarozeni(LocalDate.of(1956, 11, 22));
         System.out.println(prodejce2.getJmeno());
 
